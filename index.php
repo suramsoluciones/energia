c56450dae4679f9ba7ac250427116ca6d4b7b984
suramsoluciones <86538526+suramsoluciones@users.noreply.github.com>
2021-07-07 19:04:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <div class="container" id="main-content">
 	<h2>Bienvenidos a mi sitio web!</h2>
-	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+	<p>demos gracias al Señor!!!!!."</p>
 
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
